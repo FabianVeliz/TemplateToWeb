7df3af6db69a5218a61c4fbc7d095ef83590fafc
FabianVeliz <insignia@insignia-coreduo.(none)>
2013-12-23 17:10:37 -0300

    Creacion blog interno

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,3 +13,12 @@
       controlsContainer: '.flex-container'
     });
   });
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+ window.___gcfg = {lang: 'es'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
